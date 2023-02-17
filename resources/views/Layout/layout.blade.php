65ccc07162b32b729b0fe32696e116a26ed8909b
Alvaro Hub <102513183+Alvaro3637@users.noreply.github.com>
2023-02-17 10:58:04 +0100

    Layout mejorado

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -10,7 +10,12 @@
 </head>
 <body>
 
+@include('header');
+
 @yield('content');
+
+@include('footer');
+
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
