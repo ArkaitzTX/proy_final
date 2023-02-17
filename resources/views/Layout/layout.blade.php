511d50de6f36df6b305fa65d497a3a9ee089dbbc
ArkaitzTX <102513127+ArkaitzTX@users.noreply.github.com>
2023-02-17 12:27:59 +0100

    inicio sass

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -5,16 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.min.css">
     <title>GlichCode</title>
 </head>
 <body>
 
-@include('header');
+@include('Layout.header')
 
-@yield('content');
+@yield('content')
 
-@include('footer');
+@include('Layout.footer')
 
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,20 +1,28 @@
 @extends('Layout.layout')
-
 @section('content')
+
+<link rel="stylesheet" href="{{asset('css/inicio.css')}}">
 
 <main>
 
-    <section class="py-4 py-xl-5">
-        <div class="container">
+    {{-- Inicio --}}
+    <section id="inicio" class="py-4 py-xl-5">
+        <article class="container bg-opacity-75 bg-white rounded">
             <div class="text-center p-4 p-lg-5">
                 <p class="fw-bold text-primary mb-2">Comparte, aprende y explora</p>
                 <h1 class="fw-bold mb-4">GlichCode es una herramienta para descubrir y compartir código</h1><button class="btn btn-primary fs-5 me-2 py-2 px-4" type="button">Explora</button><button class="btn btn-light fs-5 py-2 px-4" type="button">Comparte</button>
             </div>
-        </div>
+        </article>
     </section>
-
-    <section>
-
+    {{-- Proyectos --}}
+    <section id="filtros">
+        {{-- Filtros --}}
+        <article>
+            <div class="rounded">
+                #
+            </div>
+        </article>
+        Proyectos
     </section>
 
 </main>
