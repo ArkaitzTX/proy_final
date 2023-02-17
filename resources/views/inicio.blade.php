1ab49ee3f343cd07d0a5086cd1f05a150eb7ce6a
ArkaitzTX <102513127+ArkaitzTX@users.noreply.github.com>
2023-02-17 13:24:10 +0100

    responsive

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -30,14 +30,14 @@
                 <div class="container" style="padding-left: 12px;">
                     <div class="bg-dark border rounded border-0 border-dark overflow-hidden">
                         <div class="row g-0">
-                            <div class="col-md-6">
+                            <div class="col-md-6 order-first order-md-last">
                                 <div class="text-white p-4 p-md-5">
                                     <h2 class="fw-bold text-white mb-3">{{ $proyecto->nombre }}</h2>
                                     <p class="mb-4">{{ $proyecto->descripcion }}</p>
                                     <div class="my-3"><a class="btn btn-primary btn-lg me-2" role="button" href="#">Ver</a><a class="btn btn-light btn-lg" role="button" href="#">Descargar</a></div>
                                 </div>
                             </div>
-                            <div class="col-md-6 order-first order-md-last" style="min-height: 250px;"><img class="w-100 h-100 fit-cover" src="{{url("/images/pagina/".$proyecto->img)}}"></div>
+                            <div class="col-md-6" style="min-height: 250px;"><img class="w-100 h-100 fit-cover" src="{{url("/images/pagina/".$proyecto->img)}}"></div>
                         </div>
                     </div>
                 </div>
